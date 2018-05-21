86bc03d6989be91e6cdbecb03f13de21fd269abc
Subangkar [HP-Win10] <subangkar.karmaker@gmail.com>
2018-05-21 22:33:37 +0600

    Online on Builder Pattern

---
+++ Assignment01/OnlineBuilder/src/BuilderDesign/MSResult.java
@@ -0,0 +1,12 @@
+package BuilderDesign;
+
+/**
+ * Created by Subangkar on 20-May-18.
+ */
+public class MSResult {
+	
+	public static void main( String[] args ) {
+		
+	}
+	
+}
---
+++ Assignment01/OnlineBuilder/src/BuilderDesign/ResultBuilder.java
@@ -0,0 +1,7 @@
+package BuilderDesign;
+
+/**
+ * Created by Subangkar on 20-May-18.
+ */
+public interface ResultBuilder {
+}
