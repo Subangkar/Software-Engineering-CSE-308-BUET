15b9043dd8326ea9d630fc2c3ba8b8123c11505d
Subangkar [HP-Win10] <subangkar.karmaker@gmail.com>
2018-06-18 18:06:08 +0600

    State Interface added

---
+++ Assignment02/VendorStatePattern/src/StateDesign/VendorMachineState.java
@@ -0,0 +1,13 @@
+package StateDesign;
+
+/**
+ * Created by Subangkar on 18-Jun-18.
+ */
+public interface VendorMachineState {
+	
+	void insertCoin(double value);
+	void selectDrinks(int option);
+	void deliverDrinks();
+	void changeDrinks();
+	double changeCurrency();
+}
