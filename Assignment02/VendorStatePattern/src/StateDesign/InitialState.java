8d5bba1044b8b1afe3037c80c63f79f75bfe2844
Subangkar [HP-Win10] <subangkar.karmaker@gmail.com>
2018-06-18 18:09:01 +0600

    InitialState class added

---
+++ Assignment02/VendorStatePattern/src/StateDesign/InitialState.java
@@ -0,0 +1,33 @@
+package StateDesign;
+
+/**
+ * Created by Subangkar on 18-Jun-18.
+ */
+public class InitialState implements VendorMachineState {
+	
+	
+	@Override
+	public void insertCoin( double value ) {
+	
+	}
+	
+	@Override
+	public void selectDrinks( int option ) {
+	
+	}
+	
+	@Override
+	public void deliverDrinks() {
+	
+	}
+	
+	@Override
+	public void changeDrinks() {
+	
+	}
+	
+	@Override
+	public double changeCurrency() {
+		return 0;
+	}
+}
