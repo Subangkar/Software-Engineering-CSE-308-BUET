6b3d7a460d808f18629d62a828d94917eae423a3
Subangkar [HP-Win10] <subangkar.karmaker@gmail.com>
2018-06-19 23:49:18 +0600

    Main modified SelectItemState kept

--- Assignment02/VendorStatePattern/src/StateDesign/Main.java
+++ Assignment02/VendorStatePattern/src/StateDesign/Main.java
@@ -10,9 +10,9 @@ public class Main {
 	
 	public static void main( String[] args ) {
 		DrinkSupply[] drinkSupply = { new DrinkSupply( "Dew" , 25 , 3 ) ,
-				new DrinkSupply( "RC" , 25 , 5 ) ,
-				new DrinkSupply( "Coca-Cola" , 25 , 1 ) ,
-				new DrinkSupply( "Fanta" , 25 , 7 ) , };
+				new DrinkSupply( "RC" , 20 , 5 ) ,
+				new DrinkSupply( "Coca-Cola" , 15 , 1 ) ,
+				new DrinkSupply( "Fanta" , 30 , 7 ) , };
 		
 		VendingMachine vendingMachine = new VendingMachine( drinkSupply );
 		
@@ -20,8 +20,8 @@ public class Main {
 		Scanner scanner = new Scanner( System.in );
 		int choice = 5;
 		while (choice != 0) {
-			System.out.println( "1.Insert Coin 2.Select Drink 3.Get Drink 4.Get Extra Inserted Cash\n0.Exit" );
-			System.out.print( "Enter a choice: " );
+			System.out.println( "\t1.Insert Coin 2.Select Drink 3.Get Drink 4.Get Extra Inserted Cash\n\t0.Exit" );
+			System.out.print( "\tEnter a choice: " );
 			choice = scanner.nextInt();
 			switch (choice) {
 				case 1:
